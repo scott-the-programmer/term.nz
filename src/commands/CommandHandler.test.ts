c8ad7e307893918ce3d40eefc3f8c6d428d599aa
Scott Murray <scott@murray.kiwi>
2023-04-30 21:19:01 +1200

    format command handler test

--- src/commands/CommandHandler.test.ts
+++ src/commands/CommandHandler.test.ts
@@ -5,14 +5,15 @@ describe("CommandHandler", () => {
     const commandHandler = new CommandHandler();
 
     expect(commandHandler.handleCommand("help")).toMatchInlineSnapshot(`
-      "Available commands:
-      - help: Show available commands
-      - clear: A command to clear the screen
-      - neofetch: Display system information and React logo ASCII art
-      - about: Display information about the user
-      - history: Display employment history
-      - education: Display education information"
-    `);
+"Available commands:
+- help: Show available commands
+- clear: A command to clear the screen
+- neofetch: Display system information and React logo ASCII art
+- about: Display information about the user
+- history: Display employment history
+- education: Display education information
+- spin: Weeeeeeeeeeeeeeeeeeeeee"
+`);
     expect(commandHandler.handleCommand("clear")).toEqual("clear");
     expect(commandHandler.handleCommand("neofetch")).toContain("OS:");
     expect(commandHandler.handleCommand("about")).toMatch(/clean code typer/);
