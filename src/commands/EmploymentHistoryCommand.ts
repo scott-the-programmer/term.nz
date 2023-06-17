1dd82e43c8a8a33b5c9f397761d0bc9e254f368b
Scott Murray <scott@murray.kiwi>
2023-06-17 16:23:49 +1200

    calculate current employment date based on the current date

--- src/commands/EmploymentHistoryCommand.ts
+++ src/commands/EmploymentHistoryCommand.ts
@@ -6,12 +6,6 @@ class EmploymentHistoryCommand extends Command {
 
   execute(): string {
     const employmentHistory = `
-Senior Site Reliability Engineer
-Lightspeed Commerce · Full-time
-Oct 2022 - Present · 7 mos
-Auckland, New Zealand
-Skills: Golang · Amazon Web Services (AWS) · Terraform · Apache Kafka
-
 Principal Software Engineer
 Serko Ltd. · Full-time
 Jun 2022 - Oct 2022 · 5 mos
@@ -33,18 +27,48 @@ Skills: ASP.NET · Scala · Apache Spark · Amazon Web Services (AWS) · Kuberne
 Graduate Software Engineer
 DXC Technology · Full-time
 Apr 2015 - Dec 2015 · 9 mos
-Auckland
+Auckland, New Zealand
 
 Scholarship Student
 The University of Auckland
-Nov 2014 - Feb 2015 · 4 mos
+Nov 2014 - Feb 2015 · 5 mos
 
 Teaching Assistant
 The University of Auckland
 Feb 2014 - Nov 2014 · 10 mos
+Auckland, New Zealand
 `;
 
-    return employmentHistory;
+    // This function will calculate the difference in months between two dates.
+    const diffInMonths = (date1: Date, date2: Date) => {
+      let diff = date2.getFullYear() - date1.getFullYear();
+      diff = diff * 12 + (date2.getMonth() - date1.getMonth());
+      return Math.abs(diff);
+    }
+
+    // Here we extract the start date of the latest employment.
+    const startDateString = "Oct 2022";
+
+    // Convert the month name to a number (1-12)
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const [monthStr, yearStr] = startDateString.split(' ');
+    const monthNum = monthNames.indexOf(monthStr) + 1;
+
+    const latestStartDate = new Date(Number(yearStr), monthNum - 1);
+
+    const now = new Date();
+
+    const monthsPassed = diffInMonths(latestStartDate, now);
+
+    const formattedMonths = monthsPassed > 12 ? `${Math.floor(monthsPassed/12)} yrs ${monthsPassed%12} mos` : `${monthsPassed} mos`;
+
+    const latestEmploymentEntry = `Senior Site Reliability Engineer
+Lightspeed Commerce · Full-time
+Oct 2022 - Present · ${formattedMonths}
+Auckland, New Zealand
+Skills: Golang · Amazon Web Services (AWS) · Terraform · Apache Kafka`;
+
+    return `${latestEmploymentEntry}\n${employmentHistory}`;
   }
 }
 
